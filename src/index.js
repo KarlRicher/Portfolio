80f203788c461ddefdc59ce89fbb7139afb2ee25
Karl Richer <karl.richer@outlook.com>
2023-07-27 15:24:19 -0400

    yurr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMiFhbKA30OdMUrzztQjDwM1VBKLq9kCg",
+  authDomain: "karlricher-portfolio.firebaseapp.com",
+  projectId: "karlricher-portfolio",
+  storageBucket: "karlricher-portfolio.appspot.com",
+  messagingSenderId: "620773162495",
+  appId: "1:620773162495:web:4cba5036a2729fb0bebbca",
+  measurementId: "G-R0DL20KYYG",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
